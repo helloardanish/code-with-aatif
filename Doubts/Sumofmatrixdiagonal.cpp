066e5806b6cb01c487c39835e25b0324296473d4
A R DANISH <helloardanish@gmail.com>
2023-05-06 22:56:11 +0300

    Create Sumofmatrixdiagonal.cpp

---
+++ Doubts/Sumofmatrixdiagonal.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+  int matrix[4][4] = {
+      {9, 13, 5, 2}, {1, 11, 7, 6}, {3, 7, 4, 1}, {6, 0, 7, 10}};
+
+  int row2 = sizeof(matrix) / sizeof(matrix[0]);
+  int column2 = sizeof(matrix[0]) / sizeof(int);
+
+  int sumofamatrixdiagonal = 0;
+  for (int i = 0; i < row2; i++) {
+    for (int j = 0; j < column2; j++) {
+      int temp = matrix[i][j];
+
+      if (i == j) {
+        cout << "(" << i << ", " << j << ")"
+             << " = " << temp << endl;
+        sumofamatrixdiagonal += temp;
+      }
+    }
+  }
+
+  cout << "Total sum of matrix diagonal is " << sumofamatrixdiagonal << endl;
+
+  return 0;
+}
