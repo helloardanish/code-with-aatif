abd14f1bc3f13c83f832828bfca457954557a9c0
A R DANISH <helloardanish@gmail.com>
2023-05-06 22:45:26 +0300

    Create Reversumofmatrix.cpp

---
+++ Doubts/Reversumofmatrix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+  
+
+  int matrix[4][4] = {
+      {9, 13, 5, 2}, {1, 11, 7, 6}, {3, 7, 4, 1}, {6, 0, 7, 10}};
+
+  int row2 = sizeof(matrix) / sizeof(matrix[0]);
+  int column2 = sizeof(matrix[0]) / sizeof(int);
+
+  int sumofamatrix = 0;
+  for (int i = row2-1; i >= 0; i--) {
+    for (int j = column2-1; j >= 0; j--) {
+      int temp = matrix[i][j];
+
+      sumofamatrix += temp;
+      
+      cout << "(" << i << ", " << j << ")"
+           << " = " << temp << endl;
+      
+    }
+  }
+
+
+
+  cout << "Total sum of matrix is " << sumofamatrix << endl;
+  
+  return 0;
+}
