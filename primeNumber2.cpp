cd989961d3fddc162c024500a88ace00bb4e83b5
aatifcoder <119126901+aatifcoder@users.noreply.github.com>
2023-05-10 12:40:31 +0530

    Create primeNumber2.cpp

---
+++ primeNumber2.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+   int max = 100;
+   for(int num = 1; num <= max; num++){
+       if(num == 1){
+           continue;
+       }
+       bool isPrime = true;
+       
+       for(int j = 2; j <= max / 2; j++){
+           if(num == j){
+               continue;
+           }
+          
+            if(num % j == 0){
+               isPrime = false;
+               break;
+               
+           }
+         }
+       
+           if(isPrime == true){
+               cout << num << endl;
+                }
+   }
+
+   
+
+    return 0;
+   }
