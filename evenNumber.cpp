90eded50b5eb036d376ef7c0bb01cd4923bac977
aatifcoder <119126901+aatifcoder@users.noreply.github.com>
2023-05-12 23:05:00 +0530

    Create evenNumber.cpp

---
+++ evenNumber.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"Printing even number between 1 and 100 " << endl;
+   int max = 100;
+
+   for(int i = 1; i<=max; i++){
+       if(i%2==0){
+       cout<<i<<endl;
+       }
+   }
+   
+    
+
+    return 0;
+}
